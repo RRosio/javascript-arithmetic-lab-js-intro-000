93a3525cffc2a46643188d541c78b06f3d23fdc9
Rosio G Reyes <rosio_reyes@yahoo.com>
2020-08-14 06:28:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,8 +36,13 @@ function decrement(n){
 
 function makeInt(a){
   if(typeof a === "string" && isNaN(a) != false){
-    var i = 0
-    return i
+    if(isNaN(a) != false){
+      var i = 1
+      return i
+    }else{
+      var i = 0
+      return i
+    }
   }else if (typeof a === "string"){
     return parseInt(a)
   }
